8b95a91853448712f1e82c95d452d2a85e4ccaae
DmitriyPershukov <dmitriypershukov@gmail.com>
2025-09-17 20:25:16 +0500

    made project use docker

--- recruto_test_task/settings.py
+++ recruto_test_task/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,14 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_5(+i&+^_q12t&8dikpr*w#6k!&d)q35kvc#fin30$ih%06uv1'
+#SECRET_KEY = 'django-insecure-_5(+i&+^_q12t&8dikpr*w#6k!&d)q35kvc#fin30$ih%06uv1'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-_5(+i&+^_q12t&8dikpr*w#6k!&d)q35kvc#fin30$ih%06uv1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = False
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-
+#ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
 # Application definition
 
